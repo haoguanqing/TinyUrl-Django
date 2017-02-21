f4ebc8eb9a004792c1c2ae7f32c0acca8f11345a
Guanqing <haoguanqing@gmail.com>
2017-02-20 22:52:47 -0500

    add test page 'index'

--- mysite/urls/urls.py
+++ mysite/urls/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     # ex: /urls/
+    url(r'^index/$', views.index, name='index'),
+    # ex: /urls/
     url(r'^shorten/(?P<long_url>[-\w]+)$', views.encode),
     # ex: /urls/5/
     url(r'^(?P<short_url>[0-9a-zA-Z]+)$', views.decode, name='decode'),
